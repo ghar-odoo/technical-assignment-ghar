2e7c76ce4cfc8bc78703b137aee45c57c617952f
ghar-odoo <ghar@odoo.com>
2024-03-06 18:51:27 +0530

    [FIX] stock_transport: fixed compute fields

--- stock_transport/models/stock_picking.py
+++ stock_transport/models/stock_picking.py
@@ -7,17 +7,14 @@ from odoo import api,fields, models
 class StockPicking(models.Model):
     _inherit="stock.picking"
 
-    volume = fields.Float(compute="_compute_picking_volume",string="Volume")
-    weight = fields.Float(compute="_compute_picking_weight",string="weight")
+    volume = fields.Float(compute="_compute_picking_volume_weight",string="Volume")
+    weight = fields.Float(compute="_compute_picking_volume_weight",string="weight")
 
-    @api.depends('product_id','move_ids')
-    def _compute_picking_volume(self):
+    @api.depends('move_ids')
+    def _compute_picking_volume_weight(self):
         for record in self:
-            record.volume=sum(product.product_id.volume*product.quantity for product in record.move_ids)
+            record.volume = sum(product.product_id.volume*product.quantity for product in record.move_ids)
+            record.weight = sum(product.product_id.weight*product.quantity for product in record.move_ids)
           
-    @api.depends('product_id','move_ids')
-    def _compute_picking_weight(self):
-        for record in self:
-            record.weight=sum(product.product_id.weight*product.quantity for product in record.move_ids)
-            
+  
                 
--- stock_transport/models/stock_picking_batch.py
+++ stock_transport/models/stock_picking_batch.py
@@ -7,27 +7,29 @@ from odoo import api,fields, models
 class StockPickingBatch(models.Model):
     _inherit = "stock.picking.batch"
 
-    vehicle_id = fields.Many2one("fleet.vehicle.model",string="Vehicle")
+    vehicle_id = fields.Many2one("fleet.vehicle",string="Vehicle")
     vehicle_category_id = fields.Many2one("fleet.vehicle.model.category",string="Category")
-    weight = fields.Float(string="Weight",compute="_compute_weight",store="True")
-    volume = fields.Float(string="Volume",compute='_compute_volume',store="True")
+    weight = fields.Float(string="Weight",compute="_compute_volume_weight",store="True")
+    volume = fields.Float(string="Volume",compute='_compute_volume_weight',store="True")
     dock_id = fields.Many2one("dock",string="Dock")
     transfer = fields.Integer(string="Transfer",compute="_compute_transfer",store="True")
     lines = fields.Integer(string="Lines",compute="_compute_lines",store="True")
     
     @api.depends('vehicle_category_id','picking_ids')
-    def _compute_volume(self):
-        for record in self:
-            if record.vehicle_category_id:
-                total = sum(pick.volume for pick in record.picking_ids)
-                record.volume = (total/record.vehicle_category_id.max_volume)*100
-
-    @api.depends('vehicle_category_id','picking_ids')
-    def _compute_weight(self):
+    def _compute_volume_weight(self):
         for record in self:
             if record.vehicle_category_id and record.picking_ids:
-                total = sum(pick.weight for pick in record.picking_ids)
-                record.weight = (total/record.vehicle_category_id.max_weight)*100
+                total_vol = sum(picking.volume for picking in record.picking_ids)
+                total_wei = sum(picking.weight for picking in record.picking_ids)
+                record.weight = (total_wei/record.vehicle_category_id.max_weight)*100
+                record.volume = (total_vol/record.vehicle_category_id.max_volume)*100
+
+    # @api.depends('vehicle_category_id','picking_ids')
+    # def _compute_weight(self):
+    #     for record in self:
+    #         if record.vehicle_category_id and record.picking_ids:
+    #             total = sum(picking.weight for picking in record.picking_ids)
+    #             record.weight = (total/record.vehicle_category_id.max_weight)*100
             
 
     @api.depends('move_line_ids')
@@ -45,7 +47,7 @@ class StockPickingBatch(models.Model):
         for record in self:
             record.vehicle_category_id = record.vehicle_id.category_id
 
-    @api.depends("name","weight","volume")
+    @api.depends("weight","volume")
     def _compute_display_name(self):
         for record in self:
-            record.display_name = f"{record.name} ({record.weight}Kg) ({record.volume}m\u00b3)"
+            record.display_name = f"{record.name} ({record.weight}Kg) ({record.volume}m\u00b3)  {record.vehicle_id.driver_id.name}"
